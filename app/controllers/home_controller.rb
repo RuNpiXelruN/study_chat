8f3eb745b7243835ed9467fd8c286ad0b2891a6c
Justin Davidson <jutstindavidson23@gmail.com>
2016-06-09 11:52:50 +1000

    Ordered index page users and added logic to setup profile section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+  include HomeHelper
+
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :desc)
+    @user = current_user
+    @daily_quote = INSPIRATION.sample
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_one :profile, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+
+  def time_order
+    
+  end
 end
